1ed24c894c8717ecffd7cf6052c2d3ce285a948e
Bobby Moretti <bobmoretti@gmail.com>
2025-01-12 18:19:54 -0800

    Added command line option to load the test (or other) ini

--- src/OpenRadar.py
+++ src/OpenRadar.py
@@ -1,11 +1,22 @@
 import sys
+import argparse
+import os
 
 from app import App
 
 
 def main():
     theApp = App(sys.argv)
-    theApp.on_execute()
+    parser = argparse.ArgumentParser(prog='OpenRadar', description='TacView realtime telemetry radar tool')
+
+    # set up some
+    test_ini_path = os.path.join(os.getcwd(), 'Data', 'test.ini')
+
+    parser.add_argument('-i', '--ini', nargs='?', const=test_ini_path, default=None, help='Load a test ini file.')
+
+    args = parser.parse_args()
+
+    theApp.on_execute(args)
 
 
 if __name__ == '__main__':
