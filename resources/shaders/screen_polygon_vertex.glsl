1020ac6c46392efe826522cb6662630b903f253e
Briland <briland5@users.noreply.github.com>
2025-01-12 19:55:39 -0500

    Update screen polygon shader to take in scale and width as uniforms

--- resources/shaders/screen_polygon_vertex.glsl
+++ resources/shaders/screen_polygon_vertex.glsl
@@ -6,12 +6,14 @@ layout(std430, binding = 0) buffer TVertex
 };
 
 in vec2 i_offset; // This varies per instance 
-in vec2 i_scale; // This varies per instance //px scale
-in float i_width; // This varies per instance
+// in vec2 i_scale; // This varies per instance //px scale
+// in float i_width; // This varies per instance
 in vec4 i_color; // This varies per instance
 
 uniform mat4  u_mvp;
 uniform vec2  u_resolution;
+uniform vec2  u_scale;
+uniform float u_width;
 
 out vec4 o_color;
 
@@ -24,10 +26,9 @@ void main()
     for (int i=0; i<4; ++i)
     {
         vec4 offset = vec4(i_offset, 0, 0);
-        vec4 scale = vec4(i_scale, 1, 1);
         va[i] = u_mvp * (vertex[line_i+i] + offset);
         va[i].xyz /= va[i].w;
-        va[i].xy = (va[i].xy + 1.0) * 0.5 * u_resolution + scale.xy * vertex[line_i+i].xy;
+        va[i].xy = (va[i].xy + 1.0) * 0.5 * u_resolution + u_scale.xy * vertex[line_i+i].xy;
     }
     // va is vertex[line_i], vertex[line_i+1], vertex[line_i+2], vertex[line_i+3] in screen space
     // offset and scaled
@@ -42,7 +43,7 @@ void main()
         vec2 v_miter = normalize(nv_line + vec2(-v_pred.y, v_pred.x)); // normal from 1 to 2 plus normal from 0 to 1
 
         pos = va[1];
-        pos.xy += v_miter * i_width * (tri_i == 1 ? -0.5 : 0.5) / dot(v_miter, nv_line);
+        pos.xy += v_miter * u_width * (tri_i == 1 ? -0.5 : 0.5) / dot(v_miter, nv_line);
     }
     else // 2, 4, 5 are the second triangle bordering the right edge
     {
@@ -50,7 +51,7 @@ void main()
         vec2 v_miter = normalize(nv_line + vec2(-v_succ.y, v_succ.x));
 
         pos = va[2];
-        pos.xy += v_miter * i_width * (tri_i == 5 ? 0.5 : -0.5) / dot(v_miter, nv_line);
+        pos.xy += v_miter * u_width * (tri_i == 5 ? 0.5 : -0.5) / dot(v_miter, nv_line);
     }
 
     pos.xy = pos.xy / u_resolution * 2.0 - 1.0;
