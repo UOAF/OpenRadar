f0423b9854c280e3d5f9ca28427f4de9ef70e2ac
Briland <Briland5@users.noreply.github.com>
2025-08-01 20:25:38 -0400

    remove deprecated pygame code and assets

--- src/old/messages.py
+++
@@ -1,9 +0,0 @@
-from pygame.event import custom_type
-
-UI_TEXT_ENTRY_DIALOG_TEXT_SUBMITTED = custom_type()
-UI_SETTINGS_PAGE_SERVER_CONNECT = custom_type()
-UI_SETTINGS_PAGE_SERVER_DISCONNECT = custom_type()
-DATA_THREAD_STATUS = custom_type()
-UI_SETTINGS_PAGE_REQUEST_SERVER_STATUS = custom_type()
-RADAR_SERVER_CONNECTED = custom_type()
-RADAR_SERVER_DISCONNECTED = custom_type()
